827b17249d4fc2358c73216b13b1ff9135015ab2
Agvan Grigoryan <agvangrigoryan2003@gmail.com>
2023-11-11 17:10:48 +0400

    added ex01

---
+++ c11_after_piscine/ex01/ft_map.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_map.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agvan <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/11 15:55:52 by agvan             #+#    #+#             */
+/*   Updated: 2023/11/11 17:08:21 by agvan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdlib.h>
+
+int	*ft_map(int *tab, int length, int (*f)(int))
+{
+	int	*modified_arr;
+	int	i;
+
+	i = 0;
+	modified_arr = (int *)malloc(sizeof(int) * length);
+	if (!modified_arr)
+		return (NULL);
+	while (i < length)
+	{
+		modified_arr[i] = (*f)(tab[i]);
+		i++;
+	}
+	return (modified_arr);
+}
+/*int	main(void)
+{
+	int	arr[] = {1, 2, 3, 222, -678, 0, -1, -45, 123456};
+	int	arr_len = sizeof(arr) / sizeof(arr[0]);
+
+	int *arr2 = ft_map(arr, arr_len, &ft_abs);
+	for (int i = 0; i < arr_len; i++)
+	{
+		ft_putnbr(arr2[i]);
+		ft_putchar('\n');
+	}
+	return (0);
+}*/
