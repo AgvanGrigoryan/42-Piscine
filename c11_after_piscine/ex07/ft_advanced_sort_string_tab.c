55221340ae78abb9c8ce9b08ef188c51ecb00457
agvangrigoryan <agvangrigoryan2003@gmail.com>
2023-11-14 18:56:51 +0400

    completed ex07

---
+++ c11_after_piscine/ex07/ft_advanced_sort_string_tab.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_advanced_sort_string_tab.c                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aggrigor <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 17:54:23 by aggrigor          #+#    #+#             */
+/*   Updated: 2023/11/14 17:54:33 by aggrigor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*int	ft_strcmp(char *str1, char *str2)
+{
+	int	i;
+
+	i = 0;
+	while (str1[i] && str2[i])
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (str1[i] - str2[i]);
+}*/
+void	swap_string(char **ptr1, char **ptr2)
+{
+	char	*tmp;
+
+	tmp = *ptr1;
+	*ptr1 = *ptr2;
+	*ptr2 = tmp;
+}
+
+void	ft_sort_string_tab(char **tab, int (*cmp)(char *, char *))
+{
+	int		swapped;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (tab[i + 1])
+	{
+		swapped = 0;
+		j = 1;
+		while (tab[i + j + 1])
+		{
+			if ((*cmp)(tab[j], tab[j + 1]) > 0)
+			{
+				swap_string(tab + j, tab + j + 1);
+				swapped = 1;
+			}
+			j++;
+		}
+		if (swapped == 0)
+			break ;
+		i++;
+	}
+}
+/*
+#include <stdio.h>
+int	main(int argc, char *argv[])
+{
+	(void)argc;
+	ft_sort_string_tab(argv, &ft_strcmp);
+	for (int i = 1; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}*/
