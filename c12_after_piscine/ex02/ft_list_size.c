2fb74c2a4a1d4fea1c5aa8900be096d9ee6b2d02
agvangrigoryan <agvangrigoryan2003@gmail.com>
2023-11-15 00:41:23 +0400

    ex02 done

---
+++ c12_after_piscine/ex02/ft_list_size.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_size.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aggrigor <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/15 00:20:51 by aggrigor          #+#    #+#             */
+/*   Updated: 2023/11/15 00:20:54 by aggrigor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "ft_list.h"
+
+int	ft_list_size(t_list *begin_list)
+{
+	int	count;
+
+	count = 0;
+	while (begin_list)
+	{
+		count++;
+		begin_list = begin_list->next;
+	}
+	return (count);
+}
