be8a0243da211e374ebe597d007282a3869788b9
agvangrigoryan <agvangrigoryan2003@gmail.com>
2023-11-15 10:47:41 +0400

    ex03 done

---
+++ c12_after_piscine/ex03/ft_list_last.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_last.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aggrigor <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/15 09:12:29 by aggrigor          #+#    #+#             */
+/*   Updated: 2023/11/15 09:12:34 by aggrigor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "ft_list.h"
+
+t_list	*ft_list_last(t_list *begin_list)
+{
+	while (begin_list->next)
+		begin_list = begin_list->next;
+	return (begin_list);
+}
