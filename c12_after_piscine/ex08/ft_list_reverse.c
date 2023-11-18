cf8912a196e3a08f4f9a4e438ec1e99f741c5c57
agvangrigoryan <agvangrigoryan2003@gmail.com>
2023-11-18 17:49:28 +0400

    ex08 done

---
+++ c12_after_piscine/ex08/ft_list_reverse.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_reverse.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aggrigor <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/18 16:00:23 by aggrigor          #+#    #+#             */
+/*   Updated: 2023/11/18 16:00:32 by aggrigor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stddef.h>
+
+typedef struct s_list
+{
+	struct s_list	*next;
+	void			*data;
+}	t_list;
+
+void	ft_list_reverse(t_list **begin_list)
+{
+	t_list	*next_item;
+	t_list	*prev_item;
+
+	prev_item = NULL;
+	while ((*begin_list))
+	{
+		next_item = (*begin_list)->next;
+		(*begin_list)->next = prev_item;
+		prev_item = (*begin_list);
+		(*begin_list) = next_item;
+	}
+	(*begin_list) = prev_item;
+}
