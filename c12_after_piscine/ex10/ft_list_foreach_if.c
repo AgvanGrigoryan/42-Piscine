3adebae179b7a4996ff7f0fffdd8a6866a11dd13
agvangrigoryan <agvangrigoryan2003@gmail.com>
2023-11-18 22:22:58 +0400

    done ex10

---
+++ c12_after_piscine/ex10/ft_list_foreach_if.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_foreach.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aggrigor <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/18 17:53:01 by aggrigor          #+#    #+#             */
+/*   Updated: 2023/11/18 18:44:12 by aggrigor         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "ft_list.h"
+
+void	ft_list_foreach_if(t_list *begin_list,
+		void (*f)(void *), void *data_ref, int (*cmp)())
+{
+	while (begin_list)
+	{
+		if ((*cmp)(begin_list->data, data_ref) == 0)
+			(*f)(begin_list->data);
+		begin_list = begin_list->next;
+	}
+}
