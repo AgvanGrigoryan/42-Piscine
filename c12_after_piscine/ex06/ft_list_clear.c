18f4dd9a02383dc5aaab96c6425f4c10ac608c91
agvangrigoryan <agvangrigoryan2003@gmail.com>
2023-11-19 16:03:25 +0400

    freed list

--- c12_after_piscine/ex06/ft_list_clear.c
+++ c12_after_piscine/ex06/ft_list_clear.c
@@ -9,6 +9,7 @@
 /*   Updated: 2023/11/16 09:34:03 by aggrigor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stdlib.h>
 #include "ft_list.h"
 
 void	ft_list_clear(t_list *begin_list, void (*free_fct)(void *))
@@ -18,7 +19,8 @@ void	ft_list_clear(t_list *begin_list, void (*free_fct)(void *))
 	while (begin_list)
 	{
 		next_item = begin_list->next;
-		(*free_fct)(begin_list);
+		(*free_fct)(begin_list->data);
+		free(begin_list);
 		begin_list = next_item;
 	}
 }
