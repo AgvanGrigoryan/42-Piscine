756aaf536a880120324f7fc9d6fc3aeadf2b3883
agvangrigoryan <agvangrigoryan2003@gmail.com>
2023-11-19 16:40:11 +0400

    done ex11

--- c12_after_piscine/ex11/ft_list_find.c
+++ c12_after_piscine/ex11/ft_list_find.c
@@ -9,5 +9,18 @@
 /*   Updated: 2023/11/19 12:09:11 by aggrigor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stddef.h>
+#include "ft_list.h"
 
-t_list *ft_list_find(t_list *begin_list, void *data_ref, int (*cmp)());
+t_list *ft_list_find(t_list *begin_list, void *data_ref, int (*cmp)())
+{
+	if (!begin_list || !data_ref)
+		return (NULL);
+	while (begin_list)
+	{
+		if ((*cmp)(begin_list->data, data_ref) == 0)
+			return (begin_list);
+		begin_list = begin_list->next;
+	}
+	return (NULL);
+}
